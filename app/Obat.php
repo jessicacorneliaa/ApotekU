1915844e0e1d2058b1330b8296da202f0cc99305
jessicacornelia <s160419035@student.ubaya.ac.id>
2022-06-22 10:06:28 +0700

    Transaksi Obat Terlaris

--- app/Obat.php
+++ app/Obat.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Obat extends Model
 {
+    public $timestamps = false;
+
     public function kategori(){
         return $this->belongsTo('App\Kategori', 'kategori_id');
     }
-    public $timestamps = false;
+    
+    public function transaksi(){
+        return $this->belongsToMany('App\Transaksi','transaksi_obats', 'obat_id', 'transaksi_id')->withPivot('jumlah','harga');
+    }
 }
